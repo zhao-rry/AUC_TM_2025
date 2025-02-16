344e282634da66b30bd83c27174f8e68933623ff
Harry Zhao <7owohz7@gmail.com>
2025-02-16 13:07:54 +0100

    Improved leet code

--- leet.py
+++ leet.py
@@ -1,15 +1,15 @@
 import re
 
-text = "this is a random samplE text so that i can try out different stuff with regex don't mind mE plEase i don't really even know what i am doing i'm just bored and a little confusEd by regular exprEssions but they seem really cool"
+text = "this is a random samplE text so that i cAn try out different stuff with regex don't mInd mE plEase i dOn't really even know what i am doing i'm just bored and a little confusEd by regular exprEssions but they seem really cool"
 
 # replace all "e" and "E" with "3"
 print(re.sub(r"[e|E]", "3", text)) # or r"e" followed by re.I
 
 # make a pattern that converts the entire text to leetspeak
 def to_leet(text: str) -> str:
-    leet_dict = {"a": "4", "e": "3", "o": "0", "t": "7", "s": "5"}
-    pattern = re.compile(r"[aeots]")
-    return pattern.sub(lambda m: leet_dict[m.group(0)], text)
+    leet_dict = {"a": "4", "e": "3", "i": "1", "o": "0", "t": "7", "s": "5"}
+    pattern = re.compile(r"[aeiots]", re.I)
+    return pattern.sub(lambda m: leet_dict[m.group(0).lower()], text)
 
 print(to_leet(text))
 
